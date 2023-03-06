a38e214e2ee4cfda5f6243206ea9c505d30d5a53
Depar3 <parkmin0318@gmail.com>
2023-03-06 10:45:54 +0900

    updated

--- Project2/project2/src/main/java/com/project2/project2/UserController.java
+++ Project2/project2/src/main/java/com/project2/project2/UserController.java
@@ -8,6 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
@@ -26,13 +31,22 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String loginUser(User user) {
+    public String loginUser(User user, HttpServletRequest req) {
         User existingUser = userRepository.findByUsername(user.getUsername());
         if (existingUser != null && existingUser.getPassword().equals(user.getPassword())) {
             return "redirect:/";
         } else {
+            HttpSession session = req.getSession();
+            session.setAttribute("gUserid", existingUser);
             return "login";
         }
+    }
+
+    @RequestMapping("/loginout")
+    public String doLoginout(HttpServletRequest req){
+        HttpSession session = req.getSession();
+        session.invalidate();
+        return "redirect:/";
     }
 
     @GetMapping("/register")
--- Project2/project2/src/main/webapp/WEB-INF/views/home.jsp
+++ Project2/project2/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +8,13 @@
 </head>
 <body>
     <h1>홈페이지</h1>
-    <a href="/register">회원가입 이동</a>
-    <a href="/login">로그인 이동</a>
-    <a href="/users">회원목록 이동</a>
+    <%
+        if(session.getAttribute("gUserid")==null){
+            out.print("<a href="/register">회원가입 이동</a>&nbsp;&nbsp;<a href='/login'>로그인</a>&nbsp;&nbsp;<a href="/users">회원목록 이동</a><br><br>");
+        } else{
+            out.print("<label>"+session.getAttribute("gUserid")+"</label>&nbsp;&nbsp;<a href='/signout'>로그아웃</a><br><br>");
+        }
+    %>
+    <h1>Welcome to My Home!!!</h1>
 </body>
 </html>
